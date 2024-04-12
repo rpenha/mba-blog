7ba4bc1a42e03478327d81c4fad9e60fd9ea7ef2
Rodolfo Penha <rodolfo.penha@gmail.com>
2024-04-12 18:31:42 -0300

    feat: Scroll to top on page navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import MainPanel from "./components/MainPanel";
+import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
     return (
         <>
+            <ScrollToTop/>
             <Header/>
             <MainPanel/>
             <Footer/>
