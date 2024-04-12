d901da2b9c1e60a3553cb99c20a7ea83976d46f4
Rodolfo Penha <rodolfo.penha@gmail.com>
2024-04-12 09:52:19 -0300

    feat: Favicon and head brand image

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,15 @@
+import Couch from "../../assets/images/couch-32x32.png";
+import {NavLink} from "react-router-dom";
+
 const Header = () => {
     return (
         <header className="bg-body">
             <nav className="navbar py-1 py-md-3">
                 <div className="container d-flex justify-content-center justify-content-md-start">
-                    <span className="my-2 my-md-1 h2 fw-bolder">This must be the place</span>
+                    <NavLink className="navbar-brand mx-0" to="/" >
+                        <img src={Couch} alt="Couch" className="d-inline-block my-0"/>
+                    </NavLink>
+                    <span className="mx-0 mx-md-2 my-3 my-md-1 h3 fw-bolder">This must be the place</span>
                 </div>
             </nav>
         </header>
