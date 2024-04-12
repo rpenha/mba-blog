ca3b81121d02678cf74f241e78cdfc4bedbf1716
Rodolfo Penha <rodolfo.penha@gmail.com>
2024-04-12 14:26:03 -0300

    refactor: Adjusting spacing

--- src/components/MainPanel.jsx
+++ src/components/MainPanel.jsx
@@ -4,7 +4,7 @@ import {Sidebar} from "./Sidebar.jsx";
 const MainPanel = () => {
     return (
         <main className="container p-0">
-            <div className="row d-flex mb-3 mb-md-5 mx-2 mx-md-0">
+            <div className="row d-flex mb-3 mb-md-2 mx-2 mx-md-0">
                 <Content/>
                 <Sidebar/>
             </div>
