fc29502d87c14c823a823ef4bd57a1ce6b7aad6a
Rodolfo Penha <rodolfo.penha@gmail.com>
2024-04-12 18:35:26 -0300

    fix: Post body picture margin

--- src/components/Picture.jsx
+++ src/components/Picture.jsx
@@ -1,5 +1,5 @@
 const Picture = ({title, src}) => (
-    <div className="d-flex m-0 align-items-center flex-column my-4 p-2 shadow-sm">
+    <div className="d-flex m-0 align-items-center flex-column mb-4 p-2 shadow-sm">
         <img className="mx-auto d-block img-fluid mb-1" src={src}
              alt={title}/>
         <cite className="text-muted">
