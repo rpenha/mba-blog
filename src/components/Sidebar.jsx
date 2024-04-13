b70742d34f1388472f303b7cfc5943b5bb6fab23
Rodolfo Penha <rodolfo.penha@gmail.com>
2024-04-13 19:33:15 -0300

    feat: Added home link to sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,10 +1,21 @@
 import CategoriesNavigation from "./CategoriesNavigation";
 import AuthorsNavigation from "./AuthorsNavigation";
+import {NavLink, useMatch} from "react-router-dom";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faHouse} from "@fortawesome/free-solid-svg-icons";
 
 export const Sidebar = () => {
+    const match = !useMatch("/");
     return (
         <section className="col-sm-12 col-md-3 mb-3 mb-md-0 d-none d-md-block">
             <div className="sticky-md-top d-grid gy-2">
+                {match &&
+                    <div className="py-4 ">
+                        <NavLink to="/" className="text-dark text-decoration-none">
+                            <FontAwesomeIcon icon={faHouse}/> home
+                        </NavLink>
+                    </div>
+                }
                 <div className="py-2 ">
                     <CategoriesNavigation/>
                 </div>
