00eb1a707efadb3a4ccf4859c2a1f2ce72c3ebdc
Rodolfo Penha <rodolfo.penha@gmail.com>
2024-04-12 23:51:38 -0300

    feat: New couch icon

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,18 @@
-import Couch from "../../assets/images/couch-32x32.png";
-import {NavLink} from "react-router-dom";
+import {Link} from "react-router-dom";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import { faCouch } from "@fortawesome/free-solid-svg-icons";
 
 const Header = () => {
     return (
         <header className="bg-body border-bottom shadow-sm w-100">
             <nav className="navbar py-1 py-md-3">
                 <div className="container d-flex justify-content-center justify-content-md-start">
-                    <NavLink className="navbar-brand mx-0" to="/" >
-                        <img src={Couch} alt="Couch" className="d-inline-block my-0"/>
-                    </NavLink>
-                    <span className="mx-0 mx-md-2 my-3 my-md-1 h3 fw-bolder">This must be the place</span>
+                    <Link className="btn btn-lg btn-dark" to="/" >
+                        <FontAwesomeIcon icon={faCouch} size="1x" />
+                    </Link>
+                    <span className="h3 my-0 mx-2">This must be the place</span>
+
+                    {/*<span className="mx-0 mx-md-2 my-2 my-md-1 h3 fw-bolder">This must be the place</span>*/}
                 </div>
             </nav>
         </header>
