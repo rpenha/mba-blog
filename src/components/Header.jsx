220cee542cd91191772a4c3715bf29edcc924e6a
Rodolfo Penha <rodolfo.penha@gmail.com>
2024-04-15 11:45:40 -0300

    refactor: Header organization for mobile/desktop

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,23 +1,35 @@
 import {Link} from "react-router-dom";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import { faCouch } from "@fortawesome/free-solid-svg-icons";
+import {faCouch} from "@fortawesome/free-solid-svg-icons";
 
 const Header = () => {
     return (
-        <header className="bg-body border-bottom shadow-sm w-100">
-            <nav className="navbar py-3 py-md-3">
-                <div className="container d-flex justify-content-center justify-content-md-start">
-                    <Link className="btn btn-dark d-inline-block d-md-none" to="/" >
-                        <FontAwesomeIcon icon={faCouch} size="1x" inverse/>
-                    </Link>
-                    <Link className="btn btn-lg btn-dark d-none d-md-inline-block" to="/" >
-                        <FontAwesomeIcon icon={faCouch} size="1x" inverse/>
-                    </Link>
-                    <span className="mx-2 text-nowrap fs-5 fw-bolder d-block d-md-none">[ This must be the place ]</span>
-                    <span className="mx-2 text-nowrap fs-3 fw-bolder d-none d-md-block">[ This must be the place ]</span>
-                </div>
-            </nav>
-        </header>
+        <>
+            <header className="bg-body border-bottom shadow-sm w-100 d-md-none">
+                <nav className="navbar">
+                    <div className="container d-flex justify-content-center justify-content-md-start">
+                        <Link className="btn btn btn-dark d-inline-block" to="/">
+                            <FontAwesomeIcon icon={faCouch} size="1x" inverse/>
+                        </Link>
+                        <span className="mx-2 text-nowrap fs-5 fw-bolder d-inline-block">
+                            [ This must be the place ]
+                        </span>
+                    </div>
+                </nav>
+            </header>
+            <header className="bg-body border-bottom shadow-sm w-100 d-none d-md-block">
+                <nav className="navbar">
+                    <div className="container d-flex justify-content-center justify-content-md-start">
+                        <Link className="btn btn-lg btn-dark d-inline-block" to="/">
+                            <FontAwesomeIcon icon={faCouch} size="1x" inverse/>
+                        </Link>
+                        <span className="mx-2 text-nowrap fs-3 fw-bolder d-inline-block">
+                            [ This must be the place ]
+                        </span>
+                    </div>
+                </nav>
+            </header>
+        </>
     );
 }
 
