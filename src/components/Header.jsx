050761c1df8be83ae733baa44ad3be05e755552b
Rodolfo Penha <rodolfo.penha@gmail.com>
2024-04-15 12:39:45 -0300

    refactor: Sticky header on mobile

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ import {faCouch} from "@fortawesome/free-solid-svg-icons";
 const Header = () => {
     return (
         <>
-            <header className="bg-body border-bottom shadow-sm w-100 d-md-none">
+            <header className="bg-body border-bottom shadow-sm w-100 d-md-none sticky-top">
                 <nav className="navbar">
                     <div className="container d-flex justify-content-center justify-content-md-start">
                         <Link className="btn btn btn-dark d-inline-block" to="/">
