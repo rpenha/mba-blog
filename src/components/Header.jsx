8208df8d9e1fbd29df69bab35b36a4c76a8865a9
Rodolfo Penha <rodolfo.penha@gmail.com>
2024-04-15 16:28:18 -0300

    refactor: Positioning header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ import {faCouch} from "@fortawesome/free-solid-svg-icons";
 const Header = () => {
     return (
         <>
-            <header className="d-md-none sticky-top bg-body shadow-sm">
+            <header className="d-md-none bg-body shadow-sm">
                 <nav className="navbar">
                     <div className="container d-flex justify-content-center justify-content-md-start">
                         <Link className="btn btn-sm btn-dark d-inline-block" to="/">
