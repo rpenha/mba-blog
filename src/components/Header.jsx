63314121fa8c27ff53ed24b1d3e9ab524dfd3e51
Rodolfo Penha <rodolfo.penha@gmail.com>
2024-04-15 14:51:28 -0300

    refactor: Sticky header/sidebar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,19 +5,19 @@ import {faCouch} from "@fortawesome/free-solid-svg-icons";
 const Header = () => {
     return (
         <>
-            <header className="bg-body border-bottom shadow-sm w-100 d-md-none sticky-top">
+            <header className="d-md-none sticky-top bg-body shadow-sm">
                 <nav className="navbar">
                     <div className="container d-flex justify-content-center justify-content-md-start">
-                        <Link className="btn btn btn-dark d-inline-block" to="/">
+                        <Link className="btn btn-sm btn-dark d-inline-block" to="/">
                             <FontAwesomeIcon icon={faCouch} size="1x" inverse/>
                         </Link>
-                        <span className="mx-2 text-nowrap fs-5 fw-bolder d-inline-block">
+                        <span className="mx-2 my-1 text-nowrap fs-5 fw-bolder d-inline-block">
                             [ This must be the place ]
                         </span>
                     </div>
                 </nav>
             </header>
-            <header className="bg-body border-bottom shadow-sm w-100 d-none d-md-block sticky-top">
+            <header className="d-none d-md-block sticky-top bg-body shadow-sm">
                 <nav className="navbar">
                     <div className="container d-flex justify-content-center justify-content-md-start">
                         <Link className="btn btn-lg btn-dark d-inline-block" to="/">
