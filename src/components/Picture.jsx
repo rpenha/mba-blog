f80c77940f4f7c9c232494721bbc5e6ba6770f4a
Rodolfo Penha <rodolfo.penha@gmail.com>
2024-04-14 23:52:13 -0300

    fix: Picture width

--- src/components/Picture.jsx
+++ src/components/Picture.jsx
@@ -1,6 +1,6 @@
 const Picture = ({title, src}) => (
     <div className="d-flex m-0 align-items-center flex-column mb-4 p-2 shadow-sm">
-        <img className="mx-auto d-block img-fluid mb-1" src={src}
+        <img className="mx-auto d-block img-fluid mb-1 w-100" src={src}
              alt={title}/>
         <cite className="text-muted">
             <small>{title}</small>
