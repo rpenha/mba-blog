3e61da805fe7924b5d3367e82c5de490282ce70e
Rodolfo Penha <rodolfo.penha@gmail.com>
2024-04-16 19:24:58 -0300

    fix: Home content

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -5,7 +5,7 @@ import Loading from "../components/Loading";
 const Home = () => {
     const {posts} = usePosts();
 
-    return <Loading/>;
+    return posts ? <Posts posts={posts}/> : <Loading/>;
 }
 
 export default Home;
