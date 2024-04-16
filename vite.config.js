2091e0c5cb0de6a528a5652911517477856ea365
Rodolfo Penha <rodolfo.penha@gmail.com>
2024-04-15 21:14:07 -0300

    refactor: Bundling improvements to optimize performance on page loads

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,43 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 import pluginPurgeCss from "@mojojoejo/vite-plugin-purgecss";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-      react(),
-      pluginPurgeCss()
-  ],
+    build: {
+        minify: "terser",
+        terserOptions: {
+            maxWorkers: 16,
+            compress: true
+        },
+        rollupOptions: {
+            output: {
+                compact: true,
+                manualChunks: {
+                    react: [
+                        "react",
+                        "react-dom",
+                        "react-router-dom",
+                        "react-helmet-async"
+                    ],
+                    contentful: [
+                        "contentful"
+                    ],
+                    rendering: [
+                        "@contentful/rich-text-react-renderer",
+                        "@contentful/rich-text-types"
+                    ],
+                    fontawesome: [
+                        "@fortawesome/fontawesome-svg-core",
+                        "@fortawesome/free-solid-svg-icons",
+                        "@fortawesome/react-fontawesome"
+                    ]
+                }
+            }
+        }
+    },
+    plugins: [
+        react(),
+        pluginPurgeCss()
+    ],
 })
